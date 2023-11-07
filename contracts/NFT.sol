292036a522e386c41cd54c40e060591f8d67f2dd
henryvishnevsky <github.hn9cy@passmail.com>
2023-11-07 12:38:05 -0700

    Add Mint NFTs function and tests

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -5,10 +5,15 @@ import "./ERC721Enumerable.sol";
 import "./Ownable.sol";
 
 contract NTF is ERC721Enumerable, Ownable {
+    using Strings for uint256;
+
+    string public baseURI;
+    string public baseExtention = ".json";
     uint256 public cost;
     uint256 public maxSupply;
     uint256 public allowMintingOn;
-    string public baseURI;
+
+    event Mint(uint256 amount, address minter);
 
     constructor(
         string memory _name,
@@ -17,14 +22,60 @@ contract NTF is ERC721Enumerable, Ownable {
         uint256 _maxSupply,
         uint256 _allowMintingOn,
         string memory _baseURI
+
     ) ERC721(_name, _symbol) {
         cost = _cost;
         maxSupply = _maxSupply;
         allowMintingOn = _allowMintingOn;
-        baseURI = _baseURI;
-  
+        baseURI = _baseURI; 
     }
 
+    function mint(uint256 _mintAmount) public payable {
+        //Only allow minting after specified time
+        require(block.timestamp >= allowMintingOn);
 
+        //Must mint at least 1 nft
+        require(_mintAmount > 0);
+
+        //Require enough payment
+        require(msg.value >= cost * _mintAmount);
+
+        
+        uint256 supply = totalSupply();
+
+        //Don't allow minting more tokens than available
+        require(supply + _mintAmount <= maxSupply);
+
+        //Create a token
+        for(uint256 i = 1; i <= _mintAmount; i++) {
+            _safeMint(msg.sender, supply + i);
+        }
+
+        //Emit event
+        emit Mint(_mintAmount, msg.sender);     
+    }
+
+    //Return metadata IPFS url
+    function tokenURI(uint256 _tokenId) 
+        public 
+        view 
+        virtual 
+        override 
+        returns(string memory) 
+    {
+        require(_exists(_tokenId), 'token does not exist');
+        return string(abi.encodePacked(baseURI, _tokenId.toString(), baseExtention));
+    }
+
+    function walletOfOwner(address _owner) public view returns(uint256[] memory) {
+        uint256 ownerTokenCount = balanceOf(_owner);
+        uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+
+        for(uint256 i; i < ownerTokenCount; i++) {
+            tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+        }
+
+        return tokenIds;
+    }
 
 }
